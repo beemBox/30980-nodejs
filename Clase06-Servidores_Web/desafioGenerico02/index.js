b03611bddb125e01d771324be47ffd400df2d30a
Mariano Silva <silva.mariano.s@gmail.com>
2022-05-22 20:42:46 -0300

    Desafío genérico 2

--- Clase06-Servidores_Web/desafioGenerico02/index.js
+++ Clase06-Servidores_Web/desafioGenerico02/index.js
@@ -6,3 +6,36 @@
  *
  * TODO: Mostrar por consola el puerto de escucha del servidor al momento de realizar el listen. En caso de error, representar el detalle.
  */
+
+import express from 'express'
+import moment from 'moment'
+
+// setup
+const app = express()
+const PORT = 8080
+
+// accumulators
+let visitas = 0;
+
+// En express nosotros definimos con get qué ruta queremos escuchar (endpoint)
+app.get('/', (req, res) => {
+  res.send('<h1 style="color: blue">Hola a todos desde Express</h1>')
+})
+
+app.get('/visitas', (req, res) => {
+  visitas++
+  res.send(`<h1 style="color: red">Cantidad de visitas: ${visitas}</h1>`)
+})
+
+app.get('/fyh', (req, res) => {
+  res.json({ fyh: moment().format('DD/MM/YYYY h:mm:s') }) // .json por dentro retorna un send desde el módulo de http.
+})
+
+app.get('*', (req, res) => {
+  // Este es por defecto cuando cae en cualquier otro endpoint.
+  res.send('404 - No hemos encontrado la página que buscas.')
+})
+
+app.listen(PORT, () => {
+  console.log(`Server running on Port: ${PORT}`)
+})
